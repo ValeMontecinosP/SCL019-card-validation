bc50ba83a7f6f0aa6e75f5d3b17ebbc6c6a50067
ceciPino <mepino@uc.cl>
2021-12-14 09:02:54 -0300

    actualizacion archivo js forms

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import validator from './validator.js';
+//import validator from './validator.js';
 
-console.log(validator);
+//console.log(validator);
+function saludo() {
+    let name = document.getElementById('nameperson').value;
+    document.getElementById('hello').innerHTML = 'Hola '+name;
+}
