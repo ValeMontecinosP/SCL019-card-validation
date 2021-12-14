864c5caa9e426f52aa498c45d58e94986b6bcb57
ValeMontecinosP <valemontecinosp@gmail.com>
2021-12-14 09:19:38 -0300

    actualizacion form

--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ function saludo() {
     let name = document.getElementById('nameperson').value;
     document.getElementById('hello').innerHTML = 'Hola '+name;
 }
+function myFunction() {
+  let x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
