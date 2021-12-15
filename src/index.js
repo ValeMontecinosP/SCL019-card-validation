460433afc8cce1e8edf149eb0814b9bc1e2fd19d
ceciPino <mepino@uc.cl>
2021-12-15 08:20:50 -0300

    primera demo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 //import validator from './validator.js';
 
 //console.log(validator);
+
+document.getElementsByClassName("page")[0].style.display = "block";
+document.getElementsByClassName("page")[1].style.display = "none";
+
 function saludo() {
     let name = document.getElementById('nameperson').value;
     document.getElementById('hello').innerHTML = 'Hola '+name;
+    document.getElementsByClassName("page")[0].style.display = "none";
+    document.getElementsByClassName("page")[1].style.display = "block";
+   
 }
 function myFunction() {
   let x = document.getElementById("myInput");
