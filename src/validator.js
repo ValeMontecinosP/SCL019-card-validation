f71c5513f8bdc85c88874fc0e2c99fc4d48529ba
ValeMontecinosP <valemontecinosp@gmail.com>
2021-12-31 11:38:49 -0300

    actualizacion validacion

--- src/validator.js
+++ src/validator.js
@@ -1,9 +1,9 @@
 const validator = {
-  isValid: function validar(creditCardNumber) {
+  isValid: (creditCardNumber) => {
 
     let numSum = 0;
     let value;
-    for (var i = 0; i < creditCardNumber.lenght; ++i) {
+    for (var i = 0; i < 16; ++i) {
           if (i % 2 == 0) {
               value = 2*creditCardNumber[i];
               if (value >= 10) {
@@ -17,19 +17,17 @@ const validator = {
           //console.log(value)
           numSum += value;
           console.log(numSum)
-  
+    }
+          
     if (numSum%10==0) {
           return true;
         }
         else {
           return false;
         }
-  
-    
-  }
   },
-  maskify: function enmascarar(creditCardNumber){
-    return cc.slice(0, -4).replace(/./g, '#') + cc.slice(-4);
-  }
+  //maskify: (creditCardNumber) => {
+    //return creditCardNumber.slice(0, -4).replace(/./g, '#') + creditCardNumber.slice(-4);
+  //}
 }
 export default validator;
